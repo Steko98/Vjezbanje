b4d2938e8c640be547cc1ed46d7f61d4ef781bcb
Ivan Šteko <ivan.steko5@gmail.com>
2025-07-12 12:59:48 +0200

    Pocetak stranice

--- obrtZaHortikulturu/obrtZaHortikulturu/src/App.jsx
+++ obrtZaHortikulturu/obrtZaHortikulturu/src/App.jsx
@@ -1,40 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      {/* Bootstrap Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">MySite</a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
 
-      {/* Blank Page Content */}
       <div className="container mt-5">
         <h1 className="text-center">Welcome to My Site</h1>
         <p className="text-center text-muted">This is a blank page with a Bootstrap navbar.</p>
